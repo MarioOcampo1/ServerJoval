62c07b0d57d979514b897fa6c89aa1a398519af9
Mario Ocampo <djsonicomendoza@gmail.com>
2021-12-24 10:34:08 -0300

    se armo la pagina de interferencias.

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,14 +18,39 @@ connection.connect(error => {
 
 //Settings
 //Fin de seteo de server original
-
+//Rutas Get
 router.get('/', (req, res) => {
     res.render('index.ejs');
 })
 router.get('/interferencias', (req, res) => {
-    res.render('paginas/interferencias.ejs');
+    const sql = 'SELECT c.id, c.Nombre, c.NCarpeta, c.intTelefonica, c.intAgua, c.intCloaca, c.intClaro, c.intElectricidad, c.intArnet, c.intOtros FROM clientes c';
+    res.locals.moment = moment;
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+    res.render('paginas/interferencias.ejs', { results: results });
+        }
+        else {
+            res.send('Ningun resultado encontrado');
+        }
+    })
+    
 })
+router.get('/interferencias/info',(req,res)=>{
+    const sql = 'SELECT c.id, c.Nombre, c.NCarpeta, c.intTelefonica, c.intAgua, c.intCloaca, c.intClaro, c.intElectricidad, c.intArnet, c.intOtros FROM clientes c';
+    res.locals.moment = moment;
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
 
+        if (results.length > 0) {
+            res.send(results); //en {results:results} lo que hago es guardar los resultados que envia la bd, en la variable results
+
+        }
+        else {
+            res.send('Ningun resultado encontrado');
+        }
+    })
+})
 router.get('/adminecogas', (req, res) => {
     const sql = 'SELECT c.id, c.Nombre, c.NCarpeta, t.TareaRealizada, t.ProximaTarea, t.FechaLimite FROM clientes c, adgastareas t where c.Nombre = t.Nombre  ';
     res.locals.moment = moment;
@@ -49,7 +74,7 @@ router.get('/adminecogas/info', (req, res) => {
         if (error) throw error;
 
         if (results.length > 0) {
-           res.send(results); //en {results:results} lo que hago es guardar los resultados que envia la bd, en la variable results
+            res.send(results); //en {results:results} lo que hago es guardar los resultados que envia la bd, en la variable results
 
         }
         else {
@@ -115,36 +140,7 @@ router.get('/editarContacto/:id', (req, res) => {
     })
 
 })
-router.post('/guardar', (req, res) => {
-    const Nombre = req.body.NombreCarpeta;
-    const NCarpeta = req.body.NCarpeta;
-    const Comitente = req.body.Comitente;
-    const Departamento = req.body.Departamento;
-    const DNV = req.body.DNV;
-    const DPV = req.body.DPV;
-    const IRRIGACION = req.body.IRRIGACION;
-    const HIDRAULICA = req.body.HIDRAULICA;
-    const FERROCARRIL = req.body.FERROCARRIL;
-    const OTROSPERMISOS = req.body.OTROSPERMISOS;
-    const TipoDeRed = req.body.TipoDeRed
-    // const TipoRed =req.body.Tipos-de-red;
-    const sql = 'Insert into clientes set ?';
-    connection.query(sql, {
-        Nombre: Nombre, NCarpeta: NCarpeta, Comitente: Comitente, Ubicación: Departamento, DNV: DNV, DPV: DPV, Irrigacion: IRRIGACION,
-        Hidraulica: HIDRAULICA, Ferrocarriles: FERROCARRIL, OtrosPermisos: OTROSPERMISOS, TipoDeRed: TipoDeRed
-    }, (error, results) => {
-        if (error) throw error;
 
-        if (results.length > 0) {
-            res.redirect('/adminecogas');
-        }
-        else {
-            res.redirect('/adminecogas');
-
-        }
-    })
-
-})
 router.get('/editarTareas/:id', (req, res) => {
     res.locals.moment = moment;
     const id = req.params.id;
@@ -160,14 +156,13 @@ router.get('/editarTareas/:id', (req, res) => {
         }
     })
 })
-
 router.get('/edit/:id', (req, res) => {
     res.locals.moment = moment;
     const id = req.params.id;
     const sql = 'Select * from clientes where id=?';
     connection.query(sql, [id], (error, results) => {
         if (error) throw error;
-        console.log("results tiene " +results.length+ "valores");
+        console.log("results tiene " + results.length + "valores");
         if (results.length > 0) {
             console.log("entro en el if");
 
@@ -181,6 +176,26 @@ router.get('/edit/:id', (req, res) => {
         }
     })
 })
+router.get('/historialcarpeta/:Nombre', (req, res) => {
+    const Nombre = req.params.Nombre;
+    const sql = 'SELECT * FROM historialdecambios WHERE Nombre_sub =?';
+    res.locals.moment = moment;
+    connection.query(sql, [Nombre], (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.render('paginas/historialcarpeta.ejs', { results: results }); //en {results:results} lo que hago es guardar los resultados que envia la bd, en la variable results
+
+        }
+        else {
+            res.render('paginas/historialcarpeta.ejs', { results: results });
+        }
+    })
+
+})
+router.get('/nuevocontacto', (req, res) => {
+    res.render('nuevocontacto.ejs');
+})
+//Rutas Post
 router.post('/actualizarcontacto/:id', (req, res) => {
     const id = req.body.id;
     const Nombre = req.body.Nombre;
@@ -206,12 +221,10 @@ router.post('/actualizarcontacto/:id', (req, res) => {
 
         })
 })
-router.get('/nuevocontacto', (req, res) => {
-    res.render('nuevocontacto.ejs');
-})
 router.post('/update/:id', (req, res) => {
     res.locals.moment = moment;
     const id = req.body.id;
+    console.log("id es:" + id);
     const NombreCarpeta = req.body.NombreCarpeta;
     const NCarpeta = req.body.NCarpeta
     const Comitente = req.body.Comitente;
@@ -298,98 +311,123 @@ router.post('/update/:id', (req, res) => {
 router.post('/updatetareas/:id', (req, res) => {
     res.locals.moment = moment;
     const Nombre = req.body.Nombre;
-    // const id = req.body.id;
+    const id = req.body.id;
+    console.log("id es: "+ id);
     const TareaRealizada = req.body.TareaRealizada;
     const ProximaTarea = req.body.ProximaTarea;
     const Fecha_limite = req.body.Fecha_limite;
-    let fecha= new Date();
+    const EstadoCarpeta = req.body.Estado;
+    let fecha = new Date();
     const Fecha_Tarea_sub = fecha;
-    console.log(fecha);
     const Fecha_Proxima_Tarea_sub = req.body.Fecha_limite;
-    
+    var sql = "";
     if (Fecha_limite) {
-        console.log('Nombre es: ' + Nombre);
-        console.log('fecha limite a actualizar es: ' + req.body.Fecha_limite);
-        //console.log(Tarea_Realizada: TareaRealizada, ProximaTarea: ProximaTarea);
-        var sql= 'Insert into historialdecambios set?';
-        connection.query(sql,[{Nombre_sub: Nombre, Tarea_Realizada_sub: TareaRealizada, Proxima_Tarea_sub: ProximaTarea, Fecha_Proxima_Tarea_sub:Fecha_Proxima_Tarea_sub,Fecha_Tarea_sub:Fecha_Tarea_sub}], (error, results) => {
-        if (error) throw error;
+        sql = 'Update  clientes set ? where id=?';
+        connection.query(sql, [{ Estado: EstadoCarpeta }, id], (error, results) => {
+            if (error) throw error;
+            console.log("se cargo el estado en tabla clientes");
 
-        if (results.length > 0) {
-            res.redirect('/adminecogas');
-        }
-    })
-    
+        })
+
+        sql = 'Insert into historialdecambios set?';
+        connection.query(sql, [{ Nombre_sub: Nombre, Tarea_Realizada_sub: TareaRealizada, Proxima_Tarea_sub: ProximaTarea, Fecha_Proxima_Tarea_sub: Fecha_Proxima_Tarea_sub, Fecha_Tarea_sub: Fecha_Tarea_sub }], (error, results) => {
+            if (error) throw error;
+
+            if (results.length > 0) {
+                res.redirect('/adminecogas');
+            }
+        })
+
         sql = 'Update adgastareas Set ? where Nombre =?';
-        // Insert into adgastareas(Nombre_sub,Tarea_Realizada_sub,Proxima_Tarea_sub) values(Nombre,TareaRealizada,ProximaTarea) 
-        connection.query(sql, [{TareaRealizada: TareaRealizada, ProximaTarea: ProximaTarea, FechaLimite: Fecha_limite
+        connection.query(sql, [{
+            TareaRealizada: TareaRealizada, ProximaTarea: ProximaTarea, FechaLimite: Fecha_limite
         }, Nombre], (error, results) => {
-                if (error) throw error;
+            if (error) throw error;
 
-                if (results.length > 0) {
-                    res.redirect('/adminecogas');
-                }
-                else {
-                    res.redirect('/adminecogas');
+            if (results.length > 0) {
+                res.redirect('/adminecogas');
+            }
+            else {
+                res.redirect('/adminecogas');
 
-                }
+            }
 
-            })
+        })
     }
-    else{
-        var sql= 'Insert into historialdecambios set?';
-        connection.query(sql,[{Nombre_sub: Nombre, Tarea_Realizada_sub: TareaRealizada, Proxima_Tarea_sub: ProximaTarea}], (error, results) => {
-        if (error) throw error;
+    else {
+        sql = 'Update  clientes set ? where id =?';
+        connection.query(sql, [{ Estado: EstadoCarpeta }, id], (error, results) => {
+            if (error) throw error;
+            console.log("se cargo el estado en tabla clientes");
 
-        if (results.length > 0) {
-            res.redirect('/adminecogas');
-        }
-    })
-         sql = 'Update adgastareas Set ? where Nombre =?';
-        connection.query(sql, [{TareaRealizada: TareaRealizada, ProximaTarea: ProximaTarea}, Nombre], (error, results) => {
-                if (error) throw error;
+        })
+        sql = 'Insert into historialdecambios set?';
+        connection.query(sql, [{ Nombre_sub: Nombre, Tarea_Realizada_sub: TareaRealizada, Proxima_Tarea_sub: ProximaTarea }], (error, results) => {
+            if (error) throw error;
 
-                if (results.length > 0) {
-                    res.redirect('/adminecogas');
-                }
-                else {
-                    res.redirect('/adminecogas');
+            if (results.length > 0) {
+                res.redirect('/adminecogas');
+            }
+        })
+        sql = 'Update adgastareas Set ? where Nombre =?';
+        connection.query(sql, [{ TareaRealizada: TareaRealizada, ProximaTarea: ProximaTarea }, Nombre], (error, results) => {
+            if (error) throw error;
 
-                }
+            if (results.length > 0) {
+                res.redirect('/adminecogas');
+            }
+            else {
+                res.redirect('/adminecogas');
 
-            })
+            }
+
+        })
 
     }
 }
 )
-router.get('/historialcarpeta/:Nombre', (req, res) => {
-    const Nombre= req.params.Nombre;
-    const sql = 'SELECT * FROM historialdecambios WHERE Nombre_sub =?';
-    res.locals.moment = moment;
-    connection.query(sql,[Nombre], (error, results) => {
-        if (error) throw error;
-        if (results.length > 0) {
-            res.render('paginas/historialcarpeta.ejs', { results: results}); //en {results:results} lo que hago es guardar los resultados que envia la bd, en la variable results
-
-        }
-        else {
-            res.render('paginas/historialcarpeta.ejs', { results: results});
-        }
-    })
-
-})
-router.post('/edit/delete/:id',(req,res)=>{
-    const id= req.params.id;
+router.post('/edit/delete/:id', (req, res) => {
+    const id = req.params.id;
     var sql = 'Delete FROM clientes WHERE id =?';
     sql = 'Delete FROM clientes WHERE id =?'
     res.locals.moment = moment;
-    connection.query(sql,[id], (error, results) => {
+    connection.query(sql, [id], (error, results) => {
         if (error) throw error;
         if (results.length > 0) {
-            res.render('adminecogas.ejs'); 
+            res.render('adminecogas.ejs');
         }
         else {
             res.render('adminecogas.ejs');
         }
     })
+})
+router.post('/guardar', (req, res) => {
+    const Nombre = req.body.NombreCarpeta;
+    const NCarpeta = req.body.NCarpeta;
+    const Comitente = req.body.Comitente;
+    const Departamento = req.body.Departamento;
+    const DNV = req.body.DNV;
+    const DPV = req.body.DPV;
+    const IRRIGACION = req.body.IRRIGACION;
+    const HIDRAULICA = req.body.HIDRAULICA;
+    const FERROCARRIL = req.body.FERROCARRIL;
+    const OTROSPERMISOS = req.body.OTROSPERMISOS;
+    const TipoDeRed = req.body.TipoDeRed
+    // const TipoRed =req.body.Tipos-de-red;
+    const sql = 'Insert into clientes set ?';
+    connection.query(sql, {
+        Nombre: Nombre, NCarpeta: NCarpeta, Comitente: Comitente, Ubicación: Departamento, DNV: DNV, DPV: DPV, Irrigacion: IRRIGACION,
+        Hidraulica: HIDRAULICA, Ferrocarriles: FERROCARRIL, OtrosPermisos: OTROSPERMISOS, TipoDeRed: TipoDeRed
+    }, (error, results) => {
+        if (error) throw error;
+
+        if (results.length > 0) {
+            res.redirect('/adminecogas');
+        }
+        else {
+            res.redirect('/adminecogas');
+
+        }
+    })
+
 })
